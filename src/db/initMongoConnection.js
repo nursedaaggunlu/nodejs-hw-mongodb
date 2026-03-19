672be45bbd346b11b229842d90acf79ea1fe889f
Nur Seda Ağgünlü <nursedaaggunlu@gmail.com>
2026-03-19 04:29:56 +0300

    updated 01

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -1,18 +1,14 @@
 import mongoose from 'mongoose';
 import { env } from '../utils/env.js';
 
-export async function initMongoConnection() {
+export const initMongoConnection = async () => {
   try {
-    const user = env('MONGODB_USER');
-    const pwd = env('MONGODB_PASSWORD');
-    const url = env('MONGODB_URL');
-    const db = env('MONGODB_DB');
+    const connectionString = env('MONGODB_CONNECTION_STRING');
 
-    await mongoose.connect(
-      `mongodb+srv://${user}:${pwd}@${url}/${db}?retryWrites=true&w=majority`,
-    );
+    await mongoose.connect(connectionString);
+
     console.log('Mongo connection successfully established!');
-  } catch (e) {
-    console.log(e);
+  } catch (error) {
+    console.error('Mongo connection failed:', error);
   }
-}
+};
